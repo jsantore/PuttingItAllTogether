164dc8e9c40ba294f4323b54af4b24a0dc0524cb
johns <jsantore@bridgew.edu>
2025-06-17 10:53:34 -0400

    Initial commit

---
+++ src/main/java/bsu/comp152/puttingitalltogether/MainWindowContoller.java
@@ -0,0 +1,12 @@
+package bsu.comp152.puttingitalltogether;
+
+import javafx.fxml.FXML;
+
+public class MainWindowContoller {
+
+
+    @FXML
+    protected void quitButtonClick() {
+        System.exit(0);
+    }
+}
