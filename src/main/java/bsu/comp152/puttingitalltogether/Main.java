bbad1d17ef129f2d40f3340e8269f6ea5326a63f
johns <jsantore@bridgew.edu>
2025-06-17 11:07:57 -0400

    window displays starter GUI
    not much going on yet

---
+++ src/main/java/bsu/comp152/puttingitalltogether/Main.java
@@ -0,0 +1,22 @@
+package bsu.comp152.puttingitalltogether;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage)  {
+        Scene windowPane = null;
+        FXMLLoader uiLoader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+        try {
+            windowPane = new Scene(uiLoader.load());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        stage.setScene(windowPane);
+        stage.show();
+    }
+}
